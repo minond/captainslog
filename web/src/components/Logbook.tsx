f0afa8783405e847b48a126c2b78bd9fd2b92398
Marcos Minond <minond.marcos@gmail.com>
2019-03-18 18:18:56 -0600

    web base files in place

---
+++ web/src/components/Logbook.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+export const Logbook = () =>
+  <h1>Logbook</h1>
---
+++ web/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Logbook } from "./components/Logbook";
+
+ReactDOM.render(<Logbook />, document.getElementById("body"))
