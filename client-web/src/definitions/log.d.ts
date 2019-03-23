f23ed36c4ed665d43e197c5abc60b723b06bcc7b
Marcos Minond <minond.marcos@gmail.com>
2019-03-22 21:24:42 -0600

    log definitions

---
+++ client-web/src/definitions/log.d.ts
@@ -0,0 +1,33 @@
+export type Log = {
+  guid?: string
+  text?: string
+  data?: Map<string, string>
+  createdOn?: number
+  createdBy?: string
+  updatedOn?: number
+  updatedBy?: string
+  deletedOn?: number
+  deletedBy?: string
+}
+
+export type LogCreateRequest = {
+  guid?: string
+  text?: string
+  createdOn?: number
+  updatedOn?: number
+}
+
+export type LogCreateResponse = {
+  id?: string
+  log?: {
+    guid?: string
+    text?: string
+    data?: Map<string, string>
+    createdOn?: number
+    createdBy?: string
+    updatedOn?: number
+    updatedBy?: string
+    deletedOn?: number
+    deletedBy?: string
+  }
+}
