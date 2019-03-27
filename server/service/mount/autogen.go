bda295bb7030c886c01d2c5208c5bbf13f03588e
Marcos Minond <minond.marcos@gmail.com>
2019-03-26 21:03:38 -0600

    http error statuses

--- server/service/mount/autogen.go
+++ server/service/mount/autogen.go
@@ -20,30 +20,36 @@ func MountEntryService(mux *http.ServeMux, service *service.EntryService) {
 			defer r.Body.Close()
 			data, err := ioutil.ReadAll(r.Body)
 			if err != nil {
+				http.Error(w, "unable to read request body", http.StatusBadRequest)
 				log.Printf("error reading request body: %v", err)
 				return
 			}
 
 			req := &proto.EntryCreateRequest{}
 			if err = json.Unmarshal(data, req); err != nil {
+				http.Error(w, "unable to decode request", http.StatusBadRequest)
 				log.Printf("error unmarshaling request: %v", err)
 				return
 			}
 
 			res, err := service.Create(context.Background(), req)
 			if err != nil {
+				http.Error(w, "unable to handle request", http.StatusInternalServerError)
 				log.Printf("error handling request: %v", err)
 				return
 			}
 
 			out, err := json.Marshal(res)
 			if err != nil {
+				http.Error(w, "unable to encode response", http.StatusInternalServerError)
 				log.Printf("error marshaling response: %v", err)
 				return
 			}
 
 			w.Write(out)
 
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
 }
