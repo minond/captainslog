b6a811b455e9194910250ffe227f4925909c5162
Marcos Minond <minond.marcos@gmail.com>
2019-03-28 17:55:16 -0600

    mock session

--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 
 	"github.com/minond/captainslog/server/service"
 	"github.com/minond/captainslog/server/service/mount"
@@ -23,6 +24,17 @@ func main() {
 
 	router := mux.NewRouter()
 	entryService := service.NewEntryService(db)
+
+	// TODO add real sessions with real auth
+	store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			session, _ := store.Get(r, "main")
+			session.Values["userguid"] = "e26e269c-0587-4094-bf01-108c61b0fa8a"
+			session.Save(r, w)
+			next.ServeHTTP(w, r)
+		})
+	})
 
 	mount.MountEntryService(router, entryService)
 
