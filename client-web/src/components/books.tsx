97da0516ec05415dc2a5d454a19ce7ad0f529eec
Marcos Minond <minond.marcos@gmail.com>
2019-03-31 19:24:36 -0600

    renaming component files

---
+++ client-web/src/components/books.tsx
@@ -0,0 +1,70 @@
+import * as React from "react"
+import { Component } from "react"
+
+import { css, StyleSheet } from "aphrodite"
+
+import { Book } from "../definitions/book"
+import { getBooks } from "../service/book"
+import { Entries } from "./entries"
+
+const styles = StyleSheet.create({
+  book: {
+    marginBottom: "10px",
+  }
+})
+
+interface State {
+  loaded: boolean
+  books: Book[]
+  viewing?: string
+}
+
+export class Books extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      books: [],
+      loaded: false,
+    }
+  }
+
+  componentWillMount() {
+    getBooks().then((books) =>
+      // XXX remove viewing hardcoded test
+      this.setState({ viewing: books[0].guid, loaded: true, books }))
+  }
+
+  viewBook(viewing: string) {
+    this.setState({ viewing })
+  }
+
+  render() {
+    const { loaded, books, viewing } = this.state
+
+    if (!loaded) {
+      return <h1>Loading...</h1>
+    }
+
+    const booksElem = books.map((book) => {
+      const header = (
+        <h1 className={css(styles.book)} onClick={(ev) => this.viewBook(book.guid)}>
+          {book.name}
+        </h1>
+      )
+
+      if (viewing !== book.guid) {
+        return <div key={book.guid}>{header}</div>
+      }
+
+      return (
+        <div key={book.guid}>
+          {header}
+          <Entries guid={book.guid} />
+        </div>
+      )
+    })
+
+    return <div>{booksElem}</div>
+  }
+}
--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Books } from "./components/Books";
+import { Books } from "./components/books";
 
 ReactDOM.render(<Books />, document.getElementById("body"))
