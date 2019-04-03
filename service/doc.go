38e13585e8974a0a46fdaad10865079fead1e368
Marcos Minond <minond.marcos@gmail.com>
2019-04-02 19:00:03 -0600

    service docs

---
+++ service/doc.go
@@ -0,0 +1,4 @@
+// Package service thin request handlers that call out to the rest of the
+// application. It also has a routes.json file which is used to generate
+// routes.go which is what links a request handler to a service method.
+package service
