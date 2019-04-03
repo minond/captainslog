bb881f1ca8f636cdc78a528f515b05ad5d809aaa
Marcos Minond <minond.marcos@gmail.com>
2019-04-02 18:45:38 -0600

    moving generator pragmas to file in generator folder

---
+++ generator/main.go
@@ -0,0 +1,3 @@
+//go:generate kallax gen --input ../model
+//go:generate go run ../generator/service/main.go -routes ../service/routes.json -output ../service/routes.go -package service
+package main
