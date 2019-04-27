cf5f54216ea4fd3ba7ce4c6d47f9609641a0485e
Marcos Minond <minond.marcos@gmail.com>
2019-04-27 12:23:42 -0600

    loading everything through browserify

--- client/web/src/index.tsx
+++ client/web/src/index.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import Books from "./components/books"
 import Entries from "./components/entries"
 
+require("./normalize.css")
+require("./index.css")
+
 export const Index = () => (
   <div>
     <Router>
