709f9469a9316a7b67c8b270f71f695314afe8e5
Marcos Minond <minond.marcos@gmail.com>
2019-04-27 19:31:04 -0600

    prev and next actions on date range picker

--- client/web/src/components/date_group_picker.tsx
+++ client/web/src/components/date_group_picker.tsx
@@ -18,16 +18,32 @@ type Props = {
   onChange: (_: Date) => void
 }
 
+enum UNIT {
+  DAY = 1000 * 60 * 60 * 24,
+}
+
+function add(date: Date, unit: UNIT): Date {
+  return new Date(+date + unit)
+}
+
+function sub(date: Date, unit: UNIT): Date {
+  return new Date(+date - unit)
+}
+
 export default function DateGroupPicker({ date, onChange }: Props) {
   return (
-    <DatePicker
-      selected={date}
-      className={css(styles.input)}
-      onChange={(maybeDate) => {
-        if (maybeDate) {
-          onChange(maybeDate)
-        }
-      }}
-    />
+    <div>
+      <div onClick={() => onChange(sub(date, UNIT.DAY))}>prev</div>
+      <DatePicker
+        selected={date}
+        className={css(styles.input)}
+        onChange={(maybeDate) => {
+          if (maybeDate) {
+            onChange(maybeDate)
+          }
+        }}
+      />
+      <div onClick={() => onChange(add(date, UNIT.DAY))}>next</div>
+    </div>
   )
 }
