ca8f094234708fcb955822ea5f83da942deced6d
Marcos Minond <minond.marcos@gmail.com>
2019-05-07 21:52:45 -0600

    travis config

--- query/parser_test.go
+++ query/parser_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+// var _ = `
+//
+// select reps, sets, weight
+// from workouts
+// where exercise like 'bicep'
+// or exercise like 'bench press'
+//
+// select min(weight), max(weight)
+// from workouts
+// group by exercise
+//
+// select data ->> 'exercise', min(data ->> 'weight'), max(data ->> 'weight')
+// from entries
+// where (data ->> 'weight') is not null
+// group by data ->> 'exercise'
+// ;
+//
+// `
+
 func queryMismatchMessage(expected Ast, got string) string {
 	return fmt.Sprintf(`error wth query parser.
 
