bfef6190ba0a362695a8bf70aa7dacdd56170d21
Marcos Minond <minond.marcos@gmail.com>
2019-06-06 21:09:37 -0600

    using state hooks in BookNavigation component

--- client/web/src/components/books_navigation.tsx
+++ client/web/src/components/books_navigation.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Component } from "react"
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 import BookTitle from "./book_title"
@@ -18,29 +18,22 @@ interface Props {
   active?: string
 }
 
-export default class BooksNavigation extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
+export default function BooksNavigation(props: Props) {
+  const [books, setBooks] = useState<Book[]>([])
+  const [loaded, setLoaded] = useState(false)
 
-    this.state = {
-      books: [],
-      loaded: false,
-    }
+  if (!loaded) {
+    getBooks().then((res) => {
+      setBooks(res)
+      setLoaded(true)
+    })
   }
 
-  componentWillMount() {
-    getBooks().then((books) =>
-      this.setState({ loaded: true, books }))
-  }
+  const { active } = props
+  const links = books.map((book) =>
+    <Link key={book.guid} to={`/${book.guid}`} style={link}>
+      <BookTitle name={book.name} active={book.guid === active} />
+    </Link>)
 
-  render() {
-    const { books } = this.state
-    const { active } = this.props
-    const links = books.map((book) =>
-      <Link key={book.guid} to={`/${book.guid}`} style={link}>
-        <BookTitle name={book.name} active={book.guid === active} />
-      </Link>)
-
-    return <div>{links}</div>
-  }
+  return <div>{links}</div>
 }
