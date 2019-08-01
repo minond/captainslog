8ad2344fa6b3828e754db02cd76b8e1d0e643392
Marcos Minond <minond.marcos@gmail.com>
2019-07-31 18:37:14 -0600

    combining entry components

--- client/web/src/components/entry_list.tsx
+++ client/web/src/components/entry_list.tsx
@@ -3,8 +3,6 @@ import { Component } from "react"
 
 import { Entry, EntryCreateRequest } from "../definitions/entry"
 
-import EntryLine from "./entry_line"
-
 type MaybeData = { data?: { [index: string]: string } }
 type EntryView = Entry | (EntryCreateRequest & MaybeData)
 
@@ -12,23 +10,32 @@ interface Props {
   items: EntryView[]
 }
 
+const mmap = <V, R>(xs: { [index: string]: V }, fn: (k: string, v: V, i: number) => R) =>
+  Object.keys(xs).map((k, i) => fn(k, xs[k], i))
+
+const sortByCreatedAt = (a: EntryView, b: EntryView) => {
+  if (a.createdAt === b.createdAt) {
+    return 0
+  } else if (a.createdAt > b.createdAt) {
+    return -1
+  } else {
+    return 1
+  }
+}
+
 export default function EntryList({ items }: Props) {
-  const sorted = items.sort((a, b) => {
-    if (a.createdAt === b.createdAt) {
-      return 0
-    } else if (a.createdAt > b.createdAt) {
-      return -1
-    } else {
-      return 1
-    }
+  const sorted = items.sort(sortByCreatedAt)
+  const elems = sorted.map((entry, i) => {
+    const datalist = !entry.data ? null : mmap(entry.data, (key, val, i) =>
+      <span key={i}>{key}: {val}</span>)
+
+    return (
+      <div key={entry.guid}>
+        <div>{entry.text}</div>
+        <div>{datalist}</div>
+      </div>
+    )
   })
 
-  const entries = sorted.map((entry, i) => (
-    <EntryLine
-      key={entry.guid}
-      text={entry.text}
-      data={entry.data}
-    />))
-
-  return <div>{entries}</div>
+  return <div>{elems}</div>
 }
