e696991df8eb3db4729c8554d34e064bbb023f9b
Marcos Minond <minond.marcos@gmail.com>
2019-08-10 16:27:10 -0600

    create saved query handler

--- service/saved_query.go
+++ service/saved_query.go
@@ -10,22 +10,41 @@ import (
 
 type SavedQueryService struct {
 	savedQueryStore *model.SavedQueryStore
+	userStore       *model.UserStore
 }
 
 func NewSavedQueryService(db *sql.DB) *SavedQueryService {
 	return &SavedQueryService{
 		savedQueryStore: model.NewSavedQueryStore(db),
+		userStore:       model.NewUserStore(db),
 	}
 }
 
 type SavedQueryCreateRequest struct {
+	Label   string `json:"label"`
+	Content string `json:"content"`
 }
 
 func (s SavedQueryService) Create(ctx context.Context, req *SavedQueryCreateRequest) (*model.SavedQuery, error) {
-	return nil, nil
+	user, err := getUser(ctx, s.userStore)
+	if err != nil {
+		return nil, err
+	}
+
+	savedQuery, err := model.NewSavedQuery(req.Label, req.Content, user)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = s.savedQueryStore.Insert(savedQuery); err != nil {
+		return nil, err
+	}
+
+	return savedQuery, nil
 }
 
 type SavedQueryRetrieveResponse struct {
+	Queries []*model.SavedQuery `json:"queries"`
 }
 
 func (s SavedQueryService) Retrieve(ctx context.Context, req url.Values) (*SavedQueryRetrieveResponse, error) {
