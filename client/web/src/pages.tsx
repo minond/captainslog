8144433498c5334f263ffef843c2fc22469f6910
Marcos Minond <minond.marcos@gmail.com>
2019-08-10 15:54:02 -0600

    setting page titles

--- client/web/src/pages.tsx
+++ client/web/src/pages.tsx
@@ -1,30 +1,39 @@
 import * as React from "react"
-import { ReactNode } from "react"
+import { useEffect, ReactNode } from "react"
 import { Link } from "react-router-dom"
 
 import { Books } from "./books"
 import { Entries } from "./entries"
 import { Query } from "./query"
 
+import { cachedGetBook } from "./remote"
+
 type PageProps = {
   active?: string
   children?: ReactNode
 }
 
-const Page = (props: PageProps) =>
-  <div className="page-wrapper">
-    <div className={"page-header " + (props.active ? "page-header-active" : "")}>
-      <div className="page-header-content">
-        <span className="logo">Captain's log</span>
-        <Link to="/" className={props.active === "home" ? "active" : ""}>Home</Link>
-        <Link to="/query" className={props.active === "query" ? "active" : ""}>Query</Link>
-        <Books active={props.active} />
+const Page = (props: PageProps) => {
+  useEffect(() => {
+    document.title = `Captain's Log`
+  })
+
+  return <>
+    <div className="page-wrapper">
+      <div className={"page-header " + (props.active ? "page-header-active" : "")}>
+        <div className="page-header-content">
+          <span className="logo">Captain's log</span>
+          <Link to="/" className={props.active === "home" ? "active" : ""}>Home</Link>
+          <Link to="/query" className={props.active === "query" ? "active" : ""}>Query</Link>
+          <Books active={props.active} />
+        </div>
+      </div>
+      <div className="page-content">
+        {props.children}
       </div>
     </div>
-    <div className="page-content">
-      {props.children}
-    </div>
-  </div>
+  </>
+}
 
 export const IndexPage = (props: {}) =>
   <Page active="home" />
@@ -39,7 +48,19 @@ type BookPageProps = {
   date: Date
 }
 
-export const BookPage = (props: BookPageProps) =>
-  <Page active={props.guid}>
-    <Entries bookGuid={props.guid} date={props.date} />
-  </Page>
+export const BookPage = (props: BookPageProps) => {
+  useEffect(() => {
+    document.title = "Captain's Log"
+    cachedGetBook(props.guid).then((book) => {
+      if (book) {
+        document.title = `${book.name} - Captain's Log`
+      }
+    })
+  })
+
+  return <>
+    <Page active={props.guid}>
+      <Entries bookGuid={props.guid} date={props.date} />
+    </Page>
+  </>
+}
