c09330a9fd310f2fc4c7af7afc12a6af75268908
Marcos Minond <minond.marcos@gmail.com>
2019-08-17 21:01:44 -0600

    web client snapshot tests

---
+++ client/web/src/component/date_picker.test.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { act } from "react-dom/test-utils"
+import { mount } from "enzyme"
+
+import "./testsetup"
+
+import { DatePicker, Grouping } from "./date_picker"
+
+test("DatePicker (no grouping)", () => {
+  const date = new Date(1566096667888)
+  const onChange = (_: Date) => {}
+  const grouping = Grouping.NONE
+  const component = mount(<DatePicker date={date} onChange={onChange} grouping={grouping} />)
+  expect(component).toMatchSnapshot()
+})
+
+test("DatePicker (daily grouping)", () => {
+  const date = new Date(1566096667888)
+  const onChange = (_: Date) => {}
+  const grouping = Grouping.DAY
+  const component = mount(<DatePicker date={date} onChange={onChange} grouping={grouping} />)
+  expect(component).toMatchSnapshot()
+})
