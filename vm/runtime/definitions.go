fb0068b549a4bf0b2cdb941d41c57131e943ffd1
Marcos Minond <minond.marcos@gmail.com>
2019-11-26 17:51:43 -0700

    separating definitions from pragma section

---
+++ vm/runtime/definitions.go
@@ -0,0 +1,32 @@
+package runtime
+
+import (
+	"fmt"
+
+	"github.com/minond/captainslog/vm/lang"
+)
+
+type Builtin struct {
+	lang.Value
+	fn builtinFn
+}
+
+type builtinFn func(args []lang.Expr, env *Environment) (lang.Value, error)
+
+func NewBuiltin(fn builtinFn) *Builtin                                         { return &Builtin{fn: fn} }
+func (Builtin) isValue()                                                       {}
+func (v Builtin) String() string                                               { return "#<builtin>" }
+func (v Builtin) Apply(args []lang.Expr, env *Environment) (lang.Value, error) { return v.fn(args, env) }
+
+type Procedure struct {
+	lang.Value
+	name string
+	fn   procedureFn
+}
+
+type procedureFn func(args []lang.Value) (lang.Value, error)
+
+func NewProcedure(name string, fn procedureFn) *Procedure       { return &Procedure{fn: fn, name: name} }
+func (Procedure) isValue()                                      {}
+func (v Procedure) String() string                              { return fmt.Sprintf("#<procedure:%s>", v.name) }
+func (v Procedure) Apply(args []lang.Value) (lang.Value, error) { return v.fn(args) }
--- vm/runtime/pragma.go
+++ vm/runtime/pragma.go
@@ -6,31 +6,6 @@ import (
 	"github.com/minond/captainslog/vm/lang"
 )
 
-type Builtin struct {
-	lang.Value
-	fn builtinFn
-}
-
-type builtinFn func(args []lang.Expr, env *Environment) (lang.Value, error)
-
-func NewBuiltin(fn builtinFn) *Builtin                                         { return &Builtin{fn: fn} }
-func (Builtin) isValue()                                                       {}
-func (v Builtin) String() string                                               { return "#<builtin>" }
-func (v Builtin) Apply(args []lang.Expr, env *Environment) (lang.Value, error) { return v.fn(args, env) }
-
-type Procedure struct {
-	lang.Value
-	name string
-	fn   procedureFn
-}
-
-type procedureFn func(args []lang.Value) (lang.Value, error)
-
-func NewProcedure(name string, fn procedureFn) *Procedure       { return &Procedure{fn: fn, name: name} }
-func (Procedure) isValue()                                      {}
-func (v Procedure) String() string                              { return fmt.Sprintf("#<procedure:%s>", v.name) }
-func (v Procedure) Apply(args []lang.Value) (lang.Value, error) { return v.fn(args) }
-
 var procedureSum = func(args []lang.Value) (lang.Value, error) {
 	var total float64
 	for i, n := range args {
@@ -95,7 +70,7 @@ var procedureDiv = func(args []lang.Value) (lang.Value, error) {
 	return lang.NewNumber(total), nil
 }
 
-var builtins map[string]lang.Value
+var builtins = map[string]lang.Value{}
 var procedures = map[string]procedureFn{
 	"+": procedureSum,
 	"-": procedureSub,
@@ -104,7 +79,6 @@ var procedures = map[string]procedureFn{
 }
 
 func init() {
-	builtins = make(map[string]lang.Value)
 	for name, proc := range procedures {
 		builtins[name] = NewProcedure(name, proc)
 	}
