0456424f1db667ce2279dea07dcfdedc8ce7d466
Marcos Minond <minond.marcos@gmail.com>
2019-12-14 19:15:28 -0700

    entries

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_020916) do
+ActiveRecord::Schema.define(version: 2019_12_15_021401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 2019_12_15_020916) do
     t.index ["book_id"], name: "index_collections_on_book_id"
   end
 
+  create_table "entries", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "collection_id", null: false
+    t.text "original_text"
+    t.text "processed_text"
+    t.jsonb "processed_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_entries_on_book_id"
+    t.index ["collection_id"], name: "index_entries_on_collection_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
@@ -46,4 +58,6 @@ ActiveRecord::Schema.define(version: 2019_12_15_020916) do
 
   add_foreign_key "books", "users"
   add_foreign_key "collections", "books"
+  add_foreign_key "entries", "books"
+  add_foreign_key "entries", "collections"
 end
