26c46c87a6e699a5b1ca3ee15bd40952a2be4630
Marcos Minond <minond.marcos@gmail.com>
2019-12-15 13:11:15 -0700

    user specs

---
+++ web/test/models/user_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "save happy path" do
+    user = User.new(:email => "test@test.com",
+                    :password => "xsj3k2lj4k3l2hio23321")
+
+
+    assert user.save
+  end
+end
