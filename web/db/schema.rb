8e908d0389269a46ed99ea8b9532da8a5d3ec6c3
Marcos Minond <minond.marcos@gmail.com>
2019-12-14 19:07:22 -0700

    books

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_203420) do
+ActiveRecord::Schema.define(version: 2019_12_15_020543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.integer "grouping"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_12_01_203420) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
