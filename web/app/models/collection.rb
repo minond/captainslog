b43bdfe1acb83c1a92fc4badfb9544e0cfdd0981
Marcos Minond <minond.marcos@gmail.com>
2019-12-14 19:10:10 -0700

    collections

---
+++ web/app/models/collection.rb
@@ -0,0 +1,3 @@
+class Collection < ApplicationRecord
+  belongs_to :book
+end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_020543) do
+ActiveRecord::Schema.define(version: 2019_12_15_020916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2019_12_15_020543) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.boolean "open"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_collections_on_book_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_12_15_020543) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "collections", "books"
 end
