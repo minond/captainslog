9cf31ab22348a2a233f668f9c4b2222da32ce091
Marcos Minond <minond.marcos@gmail.com>
2019-12-15 14:54:19 -0700

    book grouping enum

--- web/app/models/book.rb
+++ web/app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  after_initialize :constructor
+
+  enum :grouping => [:none, :day], :_prefix => :group_by
+
+private
+
+  def constructor
+    self.grouping ||= Book.groupings[:none]
+  end
 end
--- web/test/models/book_test.rb
+++ web/test/models/book_test.rb
@@ -5,9 +5,19 @@ class BookTest < ActiveSupport::TestCase
     assert book.save
   end
 
+  test "defaults to no grouping" do
+    assert book.group_by_none?
+  end
+
+  test "grouping can be updated" do
+    book.save
+    book.group_by_day!
+
+    assert book.group_by_day?
+  end
+
   def book(overrides = {})
     @book ||= Book.new({:user => users(:plain),
-                        :name => "Testing",
-                        :grouping => 1}.merge(overrides))
+                        :name => "Testing"}.merge(overrides))
   end
 end
