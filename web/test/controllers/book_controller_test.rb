6728fb26ea7be68ccb38070e8d03921355059bb4
Marcos Minond <minond.marcos@gmail.com>
2019-12-17 21:48:06 -0700

    using factory bot

--- web/test/controllers/book_controller_test.rb
+++ web/test/controllers/book_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class BookControllerTest < ActionDispatch::IntegrationTest
+  setup { sign_in(user) }
+
+  def user
+    @user ||= create(:user)
+  end
+
+  def book
+    @book ||= create(:book, :user => user)
+  end
+
   test "should get show page" do
-    get book_path(books(:test_log))
+    get "/book/#{book.id}"
     assert_response :success
   end
 end
---
+++ web/test/factories/book_factory.rb
@@ -0,0 +1,6 @@
+::FactoryBot.define do
+  factory :book do
+    name { "Testing" }
+    grouping { :none }
+  end
+end
