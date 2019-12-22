eef321d95bc2f20928007f362742e173b76e4a8c
Marcos Minond <minond.marcos@gmail.com>
2019-12-22 02:30:41 -0700

    system test cleanup

--- web/test/system/authentication_test.rb
+++ web/test/system/authentication_test.rb
@@ -1,29 +1,22 @@
 require "application_system_test_case"
 
 class AuthenticationTest < ApplicationSystemTestCase
-  setup do
-    Rails.application.load_seed
-  end
+  setup { Rails.application.load_seed }
 
   test "sign in" do
     sign_in
-
     assert_text "Signed in successfully."
   end
 
   test "sign out" do
     sign_in
-
     click_on "Sign out"
-
     assert_text "Signed out successfully."
   end
 
   def sign_in
     visit root_path
-
     click_on "Sign in"
-
     fill_in "Email", :with => ENV["CAPTAINS_LOG_USERNAME"]
     fill_in "Password", :with => ENV["CAPTAINS_LOG_PASSWORD"]
     click_button "Log in"
