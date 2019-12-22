6a6b1362344c847075eefc6a495553f82b7bf925
Marcos Minond <minond.marcos@gmail.com>
2019-12-22 02:32:20 -0700

    cleaning up setup test classes

--- web/test/test_helper.rb
+++ web/test/test_helper.rb
@@ -2,16 +2,12 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-class BookControllerTest < ActionDispatch::IntegrationTest
+class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 end
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
 
-  # Run tests in parallel with specified workers
   parallelize(:workers => :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
 end
