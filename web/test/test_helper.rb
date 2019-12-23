127d0b863aed0ddd56d65bf13ea5e156b0b78e3f
Marcos Minond <minond.marcos@gmail.com>
2019-12-23 14:27:25 -0700

    code coverage

--- web/test/test_helper.rb
+++ web/test/test_helper.rb
@@ -1,4 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
+
+require "simplecov"
+SimpleCov.start "rails"
+
 require_relative "../config/environment"
 require "rails/test_help"
 
@@ -10,4 +14,12 @@ class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
 
   parallelize(:workers => :number_of_processors)
+
+  parallelize_setup do |worker|
+    SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+  end
+
+  parallelize_teardown do |worker|
+    SimpleCov.result
+  end
 end
