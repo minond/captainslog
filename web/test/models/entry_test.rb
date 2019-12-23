ed5b138373d15077117ced2571ffdcf1c0657c39
Marcos Minond <minond.marcos@gmail.com>
2019-12-23 14:49:09 -0700

    entry tests

--- web/test/models/entry_test.rb
+++ web/test/models/entry_test.rb
@@ -5,12 +5,20 @@ class EntryTest < ActiveSupport::TestCase
     assert entry.save
   end
 
+  test "processed text is favored over the original text" do
+    assert_equal "a", entry(:processed_text => "a", :original_text => "b").text
+  end
+
+  test "original text is used when processed text is not set" do
+    assert_equal "b", entry(:processed_text => nil, :original_text => "b").text
+  end
+
 private
 
-  def entry
-    @entry ||= Entry.new(:book => book,
-                         :collection => collection,
-                         :original_text => "hi")
+  def entry(overrides = {})
+    @entry ||= Entry.new({ :book => book,
+                           :collection => collection,
+                           :original_text => "hi" }.merge(overrides))
   end
 
   def book
