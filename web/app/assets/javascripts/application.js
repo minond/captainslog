9b71bf850540b4d8598837867e69173cd31d3aab
Marcos Minond <minond.marcos@gmail.com>
2019-12-22 23:23:09 -0700

    moving js helper into application.js file

--- web/app/assets/javascripts/application.js
+++ web/app/assets/javascripts/application.js
@@ -1,2 +1,16 @@
 //= require jquery
 //= require jquery_ujs
+
+// Sets up an key press event listener on the entry textarea field so that
+// pressing enter/return submits the form. This givens us the best of both
+// worlds where we can have multiline input for adding multiple entries at a
+// time and also being able to submit with pressing enter on destop and mobile.
+function setupSubmitFormOnTextareaPressEnter() {
+  $("textarea[name=text]").on("keypress", function(ev) {
+    if (ev.keyCode === 13) {
+      $("form").submit()
+      ev.preventDefault()
+      return false
+    }
+  })
+}
