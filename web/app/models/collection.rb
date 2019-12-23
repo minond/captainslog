4a9444137ee28bbc58a90953d70369e0da0e285e
Marcos Minond <minond.marcos@gmail.com>
2019-12-23 14:58:55 -0700

    utc is already the default

--- web/app/models/collection.rb
+++ web/app/models/collection.rb
@@ -2,15 +2,6 @@ class Collection < ApplicationRecord
   belongs_to :book
   has_many :entries
 
-  after_initialize :constructor
-
   scope :by_book_id, ->(id) { where(:book_id => id) }
   scope :datetime_between, ->(start_time, end_time) { where("datetime between ? and ?", start_time, end_time) }
-
-private
-
-  def constructor
-    dt = datetime
-    self.datetime = dt.utc if dt
-  end
 end
