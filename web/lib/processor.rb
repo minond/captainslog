8d085f670e617a1bd21842a6b116e63dbe0407e6
Marcos Minond <minond.marcos@gmail.com>
2019-12-23 22:28:00 -0700

    cleaning up standard processing fields

--- web/lib/processor.rb
+++ web/lib/processor.rb
@@ -2,10 +2,18 @@ module Processor
   # @param [Entry] entry
   # @return [Entry]
   def self.process(entry)
-    entry.data[:_processed] = true
-    entry.data[:_processed_at] = Time.now.utc.to_i
-    entry.data[:_created_at] = entry.created_at.to_i
-    entry.data[:_updated_at] = entry.updated_at.to_i
+    entry.data = entry.data.merge(standard_fields(entry))
     entry
+  end
+
+  # @param [Entry] entry
+  # @return [Hash]
+  def self.standard_fields(entry)
+    {
+      :_processed => true,
+      :_processed_at => Time.now.utc.to_i,
+      :_created_at => entry.created_at.to_i,
+      :_updated_at => entry.updated_at.to_i
+    }
   end
 end
