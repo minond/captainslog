2048c38f7b8fafc0c0d3f8c907a372ee768098c7
Marcos Minond <minond.marcos@gmail.com>
2019-12-25 15:31:13 -0700

    processing tests

--- processor/process_test.go
+++ processor/process_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -102,4 +103,35 @@ func TestExpand_WorkoutSample(t *testing.T) {
 	}
 
 	runExpandTests("TestExpand_WorkoutSample", t, tests, shorthands)
+}
+
+func TestProcess(t *testing.T) {
+	expectedText := "Bench press, 3x10 @ 65"
+	expectedData := map[string]interface{}{
+		"exercise": "Bench press",
+		"sets":     float32(3),
+		"reps":     float32(10),
+		"weight":   float32(65),
+	}
+
+	shorthands := []Shorthand{
+		{Expansion: "xx @ ", Text: str("xx "), Match: str(`xx \d`), Priority: 1},
+		{Expansion: "3x10", Match: str("xxx"), Priority: 2},
+	}
+
+	extractors := []Extractor{
+		{Label: "exercise", Match: `^(.+),`, Type: StringData},
+		{Label: "sets", Match: `,\s{0,}(\d+)\s{0,}x`, Type: NumberData},
+		{Label: "reps", Match: `x\s{0,}(\d+)\s{0,}@`, Type: NumberData},
+		{Label: "weight", Match: `@\s{0,}(\d+)$`, Type: NumberData},
+	}
+
+	text, data, err := Process("Bench press, xxx 65", shorthands, extractors)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	} else if text != expectedText {
+		t.Errorf("unexpected text: got `%s` but expected `%s`", text, expectedText)
+	} else if !reflect.DeepEqual(data, expectedData) {
+		t.Errorf("unexpected data: got `%s` but expected `%s`", data, expectedData)
+	}
 }
