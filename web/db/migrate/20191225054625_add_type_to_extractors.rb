5b5e223150d33585bdeae421415843cefa6ede41
Marcos Minond <minond.marcos@gmail.com>
2019-12-24 22:56:14 -0700

    extractor type field

---
+++ web/db/migrate/20191225054625_add_type_to_extractors.rb
@@ -0,0 +1,5 @@
+class AddTypeToExtractors < ActiveRecord::Migration[6.0]
+  def change
+    add_column :extractors, :type, :integer, :null => false
+  end
+end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_052747) do
+ActiveRecord::Schema.define(version: 2019_12_25_054625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_12_25_052747) do
     t.bigint "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "type", null: false
     t.index ["book_id"], name: "index_extractors_on_book_id"
   end
 
