abc341ea696c1a12b26332ebb700438e9f958076
Marcos Minond <minond.marcos@gmail.com>
2019-12-25 15:26:18 -0700

    shorthand repo tests

--- processor/repository_test.go
+++ processor/repository_test.go
@@ -83,3 +83,63 @@ func TestRepository_FindExtractors(t *testing.T) {
 	assertEqual(t, firstExtractor, extractors[0])
 	assertEqual(t, secondExtractor, extractors[1])
 }
+
+var (
+	shorthandColumns = []string{"priority", "expansion", "match", "text"}
+
+	firstShorthandMatch = "a"
+	firstShorthandText  = "a"
+	firstShorthand      = Shorthand{
+		Priority:  1,
+		Expansion: "a",
+		Match:     &firstShorthandMatch,
+		Text:      &firstShorthandText,
+	}
+
+	firstShorthandRow = []driver.Value{
+		firstShorthand.Priority,
+		firstShorthand.Expansion,
+		firstShorthand.Match,
+		firstShorthand.Text,
+	}
+
+	secondShorthandMatch = "b"
+	secondShorthandText  = "b"
+	secondShorthand      = Shorthand{
+		Priority:  2,
+		Expansion: "b",
+		Match:     &secondShorthandMatch,
+		Text:      &secondShorthandText,
+	}
+
+	secondShorthandRow = []driver.Value{
+		secondShorthand.Priority,
+		secondShorthand.Expansion,
+		secondShorthand.Match,
+		secondShorthand.Text,
+	}
+)
+
+func TestRepository_FindShorthands(t *testing.T) {
+	repo, db, mock := newMockRepo(t)
+	defer db.Close()
+
+	bookID := int64(2)
+
+	rows := sqlmock.NewRows(shorthandColumns).
+		AddRow(firstShorthandRow...).
+		AddRow(secondShorthandRow...)
+
+	mock.ExpectQuery("^select (.+) from shorthands").
+		WithArgs(bookID).
+		WillReturnRows(rows)
+
+	ctx := context.TODO()
+	shorthands, err := repo.FindShorthands(ctx, bookID)
+	if err != nil {
+		t.Fatalf("unexpected error from repo.FindShorthands: %v", err)
+	}
+
+	assertEqual(t, firstShorthand, shorthands[0])
+	assertEqual(t, secondShorthand, shorthands[1])
+}
