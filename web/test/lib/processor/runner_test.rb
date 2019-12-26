99fcaf4bec3fc3866818042f6c5a06d7b198ecfd
Marcos Minond <minond.marcos@gmail.com>
2019-12-26 15:16:58 -0700

    sample response helper

--- web/test/lib/processor/runner_test.rb
+++ web/test/lib/processor/runner_test.rb
@@ -3,17 +3,21 @@ require "test_helper"
 class ProcessorRunnerTest < ActiveSupport::TestCase
   test "it raises an error when there is a processing error" do
     response = ProcessorTest::HTTPResponse.new("400", "processing error")
-    poster = ProcessorTest::Poster.new(response)
-    client = Processor::Client.new(poster)
-    runner = Processor::Runner.new(create(:entry), client)
+    runner = ProcessorTest.new_runner_with_response(response)
     assert_raises(Processor::ProcessingError) { runner.process }
   end
 
-  # test "returns processing response text and data" do
-  #   response = ProcessorTest.ok_response("hi", {"a" => "b"})
-  #   poster = ProcessorTest::Poster.new(response)
-  #   client = Processor::Client.new(poster)
-  #   runner = Processor::Runner.new(create(:entry), client)
-  #   assert_raises(Processor::ProcessingError) { runner.process }
-  # end
+  test "returns processing response text" do
+    response, expected_text, expected_data = ProcessorTest.new_sample_response
+    runner = ProcessorTest.new_runner_with_response(response)
+    returned_text, returned_data = runner.process
+    assert_equal expected_text, returned_text
+  end
+
+  test "returns processing response data" do
+    response, expected_text, expected_data = ProcessorTest.new_sample_response
+    runner = ProcessorTest.new_runner_with_response(response)
+    returned_text, returned_data = runner.process
+    assert_equal expected_data["a"], returned_data["a"]
+  end
 end
--- web/test/processor_test_helper.rb
+++ web/test/processor_test_helper.rb
@@ -23,8 +23,8 @@ module ProcessorTest
     end
   end
 
-  # @param [String] text
-  # @param [Hash] data
+  # @param [String] text, defaults to an empty string
+  # @param [Hash] data, defaults to an empty hash
   # @return [ProcessorTest::HTTPResponse]
   def self.ok_response(text = "", data = {})
     response = {
@@ -35,5 +35,24 @@ module ProcessorTest
     }
 
     ProcessorTest::HTTPResponse.new("200", response.to_json)
+  end
+
+  # @param [ProcessorTest::HTTPResponse] http_res
+  # @return [Processor::Runner]
+  def self.new_runner_with_response(http_res)
+    poster = ProcessorTest::Poster.new(http_res)
+    client = Processor::Client.new(poster)
+    Processor::Runner.new(FactoryBot.create(:entry), client)
+  end
+
+  # Creates a succssful test http response and returns this along with the used
+  # text and data.
+  #
+  # @return [Tuple<ProcessorTest::HTTPResponse, String, Hash>]
+  def self.new_sample_response
+    expected_text = "hi"
+    expected_data = {"a" => "b"}
+    response = ProcessorTest.ok_response(expected_text, expected_data)
+    [response, expected_text, expected_data]
   end
 end
