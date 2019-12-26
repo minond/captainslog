f5cbf07f8de70e71795e789409f1cdbe038ff939
Marcos Minond <minond.marcos@gmail.com>
2019-12-26 14:50:12 -0700

    testing processing error

---
+++ web/test/lib/processor/runner_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class ProcessorRunnerTest < ActiveSupport::TestCase
+  test "it raises an error when there is a processing error" do
+    response = ProcessorTest::Response.new("400", "processing error")
+    poster = ProcessorTest::Poster.new(response)
+    client = Processor::Client.new(poster)
+    runner = Processor::Runner.new(create(:entry), client)
+    assert_raises(Processor::ProcessingError) { runner.process }
+  end
+end
