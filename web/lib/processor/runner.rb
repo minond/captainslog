b253a60fe297a601bb4f4c2cc72ab8667ce96e83
Marcos Minond <minond.marcos@gmail.com>
2019-12-25 22:42:13 -0700

    wiring runner to client

--- web/lib/processor/runner.rb
+++ web/lib/processor/runner.rb
@@ -6,26 +6,40 @@ module Processor
     # @param [Entry] entry
     def initialize(entry)
       @entry = entry
+      @client = Processor::Client.new
     end
 
     # Runs entry through external text processor. Also generates standard
     # system data that it tags to the entry.
     #
+    # @raise [Processor::Error]
     # @return [Tuple<String, Hash>]
     def process
+      fail response if response.is_a?(Processor::Error)
       [processed_text, processed_fields.merge(system_fields)]
     end
 
   private
 
-    attr_reader :entry
+    attr_reader :entry, :client
+
+    # @return [Processor::Request]
+    def request
+      Processor::Request.new(:book_id => entry.book_id,
+                             :text => entry.original_text)
+    end
+
+    # @return [Processor::Response]
+    def response
+      @response ||= client.request(request)
+    end
 
     # Processes entry text through external processor service to generate
     # processed text.
     #
     # @return [Hash]
     def processed_text
-      entry.original_text
+      response.text
     end
 
     # Processes entry text through external processor service to generate
@@ -33,9 +47,7 @@ module Processor
     #
     # @return [Hash]
     def processed_fields
-      # req = Request.new(:book_id => entry.book.id,
-      #                   :text => entry.original_text)
-      {}
+      response.data
     end
 
     # @return [Hash]
