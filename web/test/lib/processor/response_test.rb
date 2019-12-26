12d025e672180c69374a974723fc3e5c82a3021d
Marcos Minond <minond.marcos@gmail.com>
2019-12-26 14:28:43 -0700

    test description is just the method name

--- web/test/lib/processor/response_test.rb
+++ web/test/lib/processor/response_test.rb
@@ -1,28 +1,28 @@
 require "test_helper"
 
 class ProcessorResponseTest < ActiveSupport::TestCase
-  test "ok? looks for 200 OK status code" do
+  test "#ok?" do
     assert Processor::Response.new(TestResponse.new("200")).ok?
     assert_not Processor::Response.new(TestResponse.new).ok?
     assert_not Processor::Response.new(TestResponse.new("400")).ok?
   end
 
-  test "code getter" do
+  test "#code" do
     assert_equal "201", Processor::Response.new(TestResponse.new("201")).code
   end
 
-  test "body getter" do
+  test "#body" do
     assert_equal "bod", Processor::Response.new(TestResponse.new("201", "bod")).body
   end
 
-  test "text parser" do
+  test "#text" do
     text = "text"
     results = { :data => { :text => text } }
     response = TestResponse.new("200", results.to_json)
     assert_equal text, Processor::Response.new(response).text
   end
 
-  test "data parse" do
+  test "#data" do
     data = { "a" => "b" }
     results = { :data => { :data => data } }
     response = TestResponse.new("200", results.to_json)
