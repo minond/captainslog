6f01334cab125c00174f063037484cf5c6b960e9
Marcos Minond <minond.marcos@gmail.com>
2019-12-26 15:29:32 -0700

    using two-line syntax to sandi meter doesn't complain about class line length

--- web/lib/processor.rb
+++ web/lib/processor.rb
@@ -2,14 +2,17 @@ require "net/http"
 require "uri"
 
 module Processor
-  class Error < StandardError; end
+  class Error < StandardError
+  end
 
   # RequestError represents an error that occured _while making_ the request to
   # the processing service.
-  class RequestError < Error; end
+  class RequestError < Error
+  end
 
   # ProcessingError represents an error _from_ the processing service. This
   # means the request was successful, but there was an error in processing the
   # text.
-  class ProcessingError < Error; end
+  class ProcessingError < Error
+  end
 end
