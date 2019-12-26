3e608dbe120f6c3361722b351cd7c149db1e0909
Marcos Minond <minond.marcos@gmail.com>
2019-12-26 01:20:31 -0700

    documenting processor param

--- web/app/jobs/process_entry_job.rb
+++ web/app/jobs/process_entry_job.rb
@@ -2,6 +2,7 @@ class ProcessEntryJob < ApplicationJob
   queue_as :default
 
   # @param [Entry] entry
+  # @param [Processor] processor, defaults to `Processor
   def perform(entry, processor = Processor)
     text, data = processor.process(entry)
     entry.text = text
