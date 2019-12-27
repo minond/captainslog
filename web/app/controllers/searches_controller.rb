0e4ffa718268f0005ca2f0214a60625a9f456960
Marcos Minond <minond.marcos@gmail.com>
2019-12-27 16:27:47 -0700

    showing search results

--- web/app/controllers/searches_controller.rb
+++ web/app/controllers/searches_controller.rb
@@ -1,4 +1,25 @@
 class SearchesController < ApplicationController
+  before_action :require_login
+
   def show
+    locals :books => books,
+           :query => query,
+           :results => results
+  end
+
+private
+
+  # @return [Array<Entry>]
+  def results
+    Entry.by_user(current_user).by_text(query)
+  end
+
+  def query
+    params[:query]
+  end
+
+  # @return [Array<Book>]
+  def books
+    @books ||= current_user.books
   end
 end
