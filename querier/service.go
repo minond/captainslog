77f22b2c1f6444ee9544f65e425c59545be328c0
Marcos Minond <minond.marcos@gmail.com>
2019-12-27 01:13:07 -0700

    querier service

--- querier/service.go
+++ querier/service.go
@@ -3,11 +3,15 @@ package main
 import (
 	"context"
 	"errors"
+
+	"github.com/minond/captainslog/querier/query"
 )
 
 var (
 	ErrReqMissingQuery  = errors.New("missing user id in request")
 	ErrReqMissingUserID = errors.New("missing query in request")
+	ErrQuerySyntax      = errors.New("query syntax error")
+	ErrQueryProcessing  = errors.New("unable to process query")
 )
 
 type Service struct {
@@ -27,7 +31,7 @@ type QueryRequest struct {
 
 type QueryResponse struct {
 	Columns []string        `json:"columns"`
-	Rows    [][]interface{} `json:"columns"`
+	Results [][]interface{} `json:"results"`
 }
 
 func (s *Service) Handle(ctx context.Context, req *QueryRequest) (*QueryResponse, error) {
@@ -35,7 +39,27 @@ func (s *Service) Handle(ctx context.Context, req *QueryRequest) (*QueryResponse
 		return nil, err
 	}
 
-	return nil, nil
+	unsafe, err := query.Parse(req.Query)
+	if err != nil {
+		return nil, ErrQuerySyntax
+	}
+
+	sanitized, err := Convert(unsafe, req.UserID)
+	if err != nil {
+		return nil, ErrQueryProcessing
+	}
+
+	columns, results, err := s.repo.Execute(ctx, sanitized.String())
+	if err != nil {
+		return nil, err
+	}
+
+	response := &QueryResponse{
+		Columns: columns,
+		Results: results,
+	}
+
+	return response, nil
 }
 
 func validateServiceRequest(req *QueryRequest) error {
