7e2a9c6a066d344d0b412c8615895845ce67bc9a
Marcos Minond <minond.marcos@gmail.com>
2019-12-26 20:34:49 -0700

    user data helper

--- web/app/models/entry.rb
+++ web/app/models/entry.rb
@@ -17,6 +17,13 @@ class Entry < ApplicationRecord
     self.processed_text = text
   end
 
+  # Human friendly data
+  #
+  # @return [Hash]
+  def user_data
+    @user_data ||= data.filter { |key, _val| !key.starts_with?("_") }.sort
+  end
+
 private
 
   def constructor
