c2f5de7b47eb107bca587c9a7aaed9327b0e27e8
Marcos Minond <minond.marcos@gmail.com>
2019-12-27 19:06:12 -0700

    fixing session routes

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root :to => "pages#home"
 
+  devise_for :users, :skip => [:registrations, :sessions]
   as :user do
     post "/users/sign_in" => "devise/sessions#create", :as => :user_session
     delete "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
   end
 
-  resource :search, :only => [:show]
-
+  get "/book/:id/:requested_time", :to => "book#show", :as => :book_at
   resources :book, :only => [:show] do
     resource :entry, :only => [:create]
   end
 
-  get "/book/:id/:requested_time", :to => "book#show", :as => :book_at
+  resource :search, :only => [:show]
 end
