8130f987fcd69527f63eae9ca068772ccd745b4a
Marcos Minond <minond.marcos@gmail.com>
2019-12-27 19:35:07 -0700

    better pages controller tests

--- web/test/controllers/pages_controller_test.rb
+++ web/test/controllers/pages_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home page" do
+  test "renders the welcome page when there is no session" do
     get "/"
-    assert_response :success
+    assert response.body.include? "is an application for logging"
+  end
+
+  test "renders the home page when there is a session" do
+    sign_in user
+    get "/"
+    assert response.body.include? "Sign out"
   end
 end
