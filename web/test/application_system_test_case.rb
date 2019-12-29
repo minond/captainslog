d67359b006bc52f52186d6886c3643a54d916a0d
Marcos Minond <minond.marcos@gmail.com>
2019-12-28 22:39:36 -0700

    fixing system test

--- web/test/application_system_test_case.rb
+++ web/test/application_system_test_case.rb
@@ -2,4 +2,20 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, :using => :chrome, :screen_size => [1400, 1400]
+
+  def sign_in
+    visit root_path
+    fill_in "Email", :with => ENV["CAPTAINS_LOG_USERNAME"]
+    fill_in "Password", :with => ENV["CAPTAINS_LOG_PASSWORD"]
+    click_button "Sign in"
+  end
+
+  def sign_out
+    go_to_user_page
+    click_on "Sign out"
+  end
+
+  def go_to_user_page
+    find("[data-testing=userpage]").click
+  end
 end
--- web/test/system/authentication_test.rb
+++ web/test/system/authentication_test.rb
@@ -10,15 +10,7 @@ class AuthenticationTest < ApplicationSystemTestCase
 
   test "sign out" do
     sign_in
-    click_on "Sign out"
+    sign_out
     assert_text "Signed out successfully."
-  end
-
-  def sign_in
-    visit root_path
-    click_on "Sign in"
-    fill_in "Email", :with => ENV["CAPTAINS_LOG_USERNAME"]
-    fill_in "Password", :with => ENV["CAPTAINS_LOG_PASSWORD"]
-    click_button "Log in"
   end
 end
