2d70d2f326b32dffec5010cd7533129b7df5eea8
Marcos Minond <minond.marcos@gmail.com>
2020-01-01 12:48:00 -0700

    entry show page

--- web/app/controllers/entry_controller.rb
+++ web/app/controllers/entry_controller.rb
@@ -2,10 +2,23 @@ class EntryController < ApplicationController
   before_action :require_login
 
   # === URL
+  #   GET /entry/:id
+  #
+  # === Request fields
+  #   [Integer] id - the entry id for the entry to show
+  #
+  # === Sample request
+  #   /entry/1
+  #
+  def show
+    locals :entry => current_entry
+  end
+
+  # === URL
   #   DELETE /entry/:id
   #
   # === Request fields
-  #   [Integer] id - the netry id for the entry to delete
+  #   [Integer] id - the entry id for the entry to delete
   #
   # === Sample request
   #   /entry/1
