742874e237a6138bf1c6be5fb18a1b62a5e78089
Marcos Minond <minond.marcos@gmail.com>
2020-01-08 19:14:38 -0700

    book creation tests

--- web/test/controllers/book_controller_test.rb
+++ web/test/controllers/book_controller_test.rb
@@ -3,6 +3,11 @@ require "test_helper"
 class BookControllerTest < ActionDispatch::IntegrationTest
   setup { sign_in user }
 
+  test "show get the new page" do
+    get "/book/new"
+    assert_response :success
+  end
+
   test "should get show page" do
     get "/book/#{book.slug}"
     assert_response :success
@@ -12,5 +17,29 @@ class BookControllerTest < ActionDispatch::IntegrationTest
     assert_changes -> { Entry.count } do
       post "/book/#{book.slug}/entry", :params => { :text => "hi" }
     end
+  end
+
+  test "succesfully creating a new book" do
+    assert_changes -> { Book.count } do
+      post "/book", :params => {
+        :book => {
+          :name => "Testing",
+          :slug => "testing",
+          :grouping => :none,
+        }
+      }
+    end
+  end
+
+  test "failure in creating the book results in an error message" do
+    post "/book", :params => {
+      :book => {
+        :name => book.name,
+        :slug => book.slug,
+        :grouping => book.grouping,
+      }
+    }
+
+    assert response.body.include? "There was an error creating the book."
   end
 end
