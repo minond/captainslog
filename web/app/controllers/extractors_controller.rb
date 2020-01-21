65aee615528b0640e23b5e4d8bbfd58618641efc
Marcos Minond <minond.marcos@gmail.com>
2020-01-20 18:35:56 -0700

    loading current resources

--- web/app/controllers/extractors_controller.rb
+++ web/app/controllers/extractors_controller.rb
@@ -1,5 +1,18 @@
 class ExtractorsController < ApplicationController
   before_action :require_login
 
-  def show; end
+  def show
+    locals :extractor => current_extractor
+  end
+
+private
+
+  # Find and return the current "active" extractor. This is scopes to the
+  # user's extractors.
+  #
+  # @return [Extractor]
+  def current_extractor
+    @current_extractor ||= Extractor.find_by!(:user => current_user,
+                                              :id => params[:id])
+  end
 end
--- web/app/controllers/shorthands_controller.rb
+++ web/app/controllers/shorthands_controller.rb
@@ -1,5 +1,18 @@
 class ShorthandsController < ApplicationController
   before_action :require_login
 
-  def show; end
+  def show
+    locals :shorthand => current_shorthand
+  end
+
+private
+
+  # Find and return the current "active" shorthand. This is scopes to the
+  # user's shorthands.
+  #
+  # @return [Shorthand]
+  def current_shorthand
+    @current_shorthand ||= Shorthand.find_by!(:user => current_user,
+                                              :id => params[:id])
+  end
 end
