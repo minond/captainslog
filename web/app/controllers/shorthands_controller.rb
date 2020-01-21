eb24b44563c315c1c426087c0dab3140a9be8edf
Marcos Minond <minond.marcos@gmail.com>
2020-01-20 21:35:22 -0700

    shorthand update endpoint

--- web/app/controllers/shorthands_controller.rb
+++ web/app/controllers/shorthands_controller.rb
@@ -1,8 +1,32 @@
 class ShorthandsController < ApplicationController
   before_action :require_login
 
+  # === URL
+  #   GET /shorthands/:id
+  #
+  # === Sample request
+  #   /shorthands/1
+  #
   def show
     locals :shorthand => current_shorthand
+  end
+
+  # === URL
+  #   PATCH /shorthands/:id
+  #
+  # === Request fields
+  #   [Integer] shorthand[priority] - priority number
+  #   [String] shorthand[expansion] - the shorthand expansion
+  #   [String] shorthand[match] - the shorthand match
+  #   [String] shorthand[text] - the shorthand text
+  #
+  # === Sample request
+  #   /shorthands/:id?
+  #
+  def update
+    ok = update_shorthand
+    notify(ok, :successful_shorthand_update, :failure_in_shorthand_update)
+    ok ? redirect_to(current_shorthand) : locals(:show, :shorthand => current_shorthand)
   end
 
 private
@@ -14,5 +38,19 @@ private
   def current_shorthand
     @current_shorthand ||= Shorthand.find_by!(:user => current_user,
                                               :id => params[:id])
+  end
+
+  # Update the shorthand and return true if there were not errors doing so.
+  #
+  # @return [Boolean]
+  def update_shorthand
+    current_shorthand.update(permitted_shorthand_params)
+    current_shorthand.errors.empty?
+  end
+
+  # @return [ActionController::Parameters]
+  def permitted_shorthand_params
+    params.require(:shorthand)
+          .permit(:priority, :expansion, :match, :text)
   end
 end
--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   # === URL
-  #   GET /user
+  #   PATCH /user
   #
   # === Request fields
   #   [String] user[name] - the name
