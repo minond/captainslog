445dc0442f79fa8eab255929e6cf5734d0df260f
Marcos Minond <minond.marcos@gmail.com>
2020-02-15 17:56:28 -0700

    cleaning up network module a bit

--- mobile/network.ts
+++ mobile/network.ts
@@ -1,30 +1,41 @@
-export type endpoint = "token" | "books"
-const BASE = "https://logs.minond.xyz"
-// const BASE = "http://192.168.1.6:3000"
-const API: { [x in endpoint]: string } = {
+enum Header {
+  Accept = "Accept",
+  ContentType = "ContentType",
+}
+
+enum MimeType {
+  Json = "application/json"
+}
+
+type endpoint = "token" | "books"
+type endpointValues = { [x in endpoint]: string }
+const Base = "https://logs.minond.xyz"
+const Endpoint: endpointValues = {
   token: "/api/v1/token",
   books: "/api/v1/books",
 }
 
-const url = (ep: endpoint): string =>
-  BASE + API[ep]
+const urlFor = (ep: endpoint): string =>
+  Base + Endpoint[ep]
+
+function jsonPost(url: string, content: {}) {
+  const method = "POST"
+  const body = JSON.stringify(content)
+  const headers = {
+    [Header.Accept]: MimeType.Json,
+    [Header.ContentType]: MimeType.Json,
+  }
+
+  return fetch(url, { method, body, headers })
+}
 
 export async function createToken(email: string, password: string): Promise<string> {
-  const response = await fetch(url("token"), {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-
+  const response = await jsonPost(urlFor("token"), { email, password })
   if (!response) {
     throw new Error("empty response")
   }
 
   const body = await response.json()
-
   if (!body.token) {
     throw new Error("bad response")
   }
