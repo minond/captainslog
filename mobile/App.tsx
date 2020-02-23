b170f3415d7f9f33664e053dfd38945bcc352f77
Marcos Minond <minond.marcos@gmail.com>
2020-02-22 17:51:40 -0700

    authenticated view

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,42 +1,25 @@
 import React, { useState, useEffect } from "react"
 import { Text, View } from "react-native"
 
-import Login from "./src/view/Login"
-import styles from "./src/styles"
+import AppView from "./src/component/AppView"
+import Button from "./src/component/Button"
+import Authenticated from "./src/view/Authenticated"
 
-import { createToken, getToken, setToken } from "./src/repository/token"
+import { clearToken, createToken, getToken, setToken } from "./src/repository/token"
 
 export default function App() {
-  const [loadingToken, setLoadingToken] = useState(true)
-  const [sessionToken, setSessionToken] = useState<string | null>(null)
-
-  useEffect(() => {
-    getToken().then((token) => {
-      if (token && typeof token === "string") {
-        setSessionToken(token)
-      }
-
-      setLoadingToken(false)
-    }).catch(() => setLoadingToken(false))
-  })
-
-  if (loadingToken) {
-    return (
-      <View style={styles.containerWrapper}>
-        <Text>loading...</Text>
-      </View>
-    )
-  } else if (!sessionToken) {
-    return (
-      <View style={styles.containerWrapper}>
-        <Login setToken={setToken} createToken={createToken} afterLogin={setSessionToken} />
-      </View>
-    )
-  } else {
-    return (
-      <View style={styles.containerWrapper}>
-        <Text>ok.</Text>
-      </View>
-    )
-  }
+  return (
+    <Authenticated
+      clearToken={clearToken}
+      createToken={createToken}
+      getToken={getToken}
+      setToken={setToken}
+    >
+      {(sessionToken, logout) =>
+        <AppView>
+          <Text>ok</Text>
+          <Button label="logout" onPress={logout} />
+        </AppView>}
+    </Authenticated>
+  )
 }
--- mobile/src/view/Authenticated.tsx
+++ mobile/src/view/Authenticated.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react"
-import { Text, TextInput } from "react-native"
+import React, { ReactElement, useState, useEffect } from "react"
+import { Text, TextInput, View } from "react-native"
 
-import { CreateToken, SetToken } from "../repository/token"
-import styles from "../styles"
-import strings from "../strings"
+import { ClearToken, CreateToken, GetToken, SetToken } from "../repository/token"
 
 import AppView from "../component/AppView"
 import Button from "../component/Button"
 import FormField from "../component/FormField"
 import Header from "../component/Header"
 
+import styles from "../styles"
+import strings from "../strings"
+
+type AuthenticatedChild = (token: string, logout: () => void) => ReactElement
 type AfterLogin = (token: string) => void
+type StringSetter = (_: string | null) => void
+
+const logout = (
+  clearToken: ClearToken,
+  setSessionToken: StringSetter
+) => {
+  clearToken().then(() => setSessionToken(null))
+}
 
 const login = (
   email: string,
@@ -41,18 +51,59 @@ const login = (
     .catch((err) => setErrorMessage(strings.invalidLogin))
 }
 
-type LoginProps = {
+type AuthenticatedProps = {
+  setToken: SetToken
+  createToken: CreateToken
+  clearToken: ClearToken
+  getToken: GetToken
+  children: AuthenticatedChild
+}
+
+export default function Authenticated(props: AuthenticatedProps) {
+  const [loadingToken, setLoadingToken] = useState(true)
+  const [sessionToken, setSessionToken] = useState<string | null>(null)
+
+  const onLogout = () =>
+    logout(
+      props.clearToken,
+      setSessionToken,
+    )
+
+  useEffect(() => {
+    props.getToken().then((token) => {
+      if (token && typeof token === "string") {
+        setSessionToken(token)
+      }
+
+      setLoadingToken(false)
+    }).catch(() => setLoadingToken(false))
+  })
+
+  if (loadingToken) {
+    return (
+      <AppView>
+        <Text>loading...</Text>
+      </AppView>
+    )
+  } else if (!sessionToken) {
+    return <LoginForm setToken={props.setToken} createToken={props.createToken} afterLogin={setSessionToken} />
+  } else {
+    return props.children(sessionToken, onLogout)
+  }
+}
+
+export type LoginFormProps = {
   setToken: SetToken
   createToken: CreateToken
   afterLogin: AfterLogin
 }
 
-export default function Login(props: LoginProps) {
+function LoginForm(props: LoginFormProps) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [errorMessage, setErrorMessage] = useState("")
 
-  const onLoginButtonPress = () =>
+  const onLogin = () =>
     login(
       email,
       password,
@@ -88,7 +139,7 @@ export default function Login(props: LoginProps) {
       </FormField>}
 
     <FormField>
-      <Button label={strings.login} onPress={onLoginButtonPress} />
+      <Button label={strings.login} onPress={onLogin} />
     </FormField>
   </AppView>
 }
