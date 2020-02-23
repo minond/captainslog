8152ee3c7a0117c3345f78ba0124b765c78ea6c0
Marcos Minond <minond.marcos@gmail.com>
2020-02-23 15:53:41 -0700

    fitbit client

---
+++ web/lib/puller/fitbit.rb
@@ -0,0 +1,88 @@
+class Puller::Fitbit < Puller::OauthClient
+  Steps = Struct.new(:date, :value, :keyword_init => true) do
+    # @param [Hash] result
+    # @return [Steps]
+    def self.from_result(result)
+      new(:date => Date.parse(result["dateTime"]),
+          :value => result["value"].to_i)
+    end
+  end
+
+  HeartRate = Struct.new(:date, :value, :keyword_init => true) do
+    # @param [Hash] result
+    # @return [HeartRate]
+    def self.from_result(result)
+      new(:date => Date.parse(result["dateTime"]),
+          :value => result["value"]["restingHeartRate"])
+    end
+  end
+
+  # @param [Hash] options
+  def initialize(options = {})
+    client(options)
+  end
+
+  # @return [String]
+  def auth_url
+    client.auth_url
+  end
+
+  # @param [String] code
+  def code=(code)
+    client.get_token(code)
+    @user_id = client.token["user_id"]
+    nil
+  end
+
+  # @return [Hash]
+  def serialize_token
+    {
+      :user_id => user_id,
+      :access_token => client.token.token,
+      :refresh_token => client.token.refresh_token,
+      :expires_at => client.token.expires_at,
+    }
+  end
+
+  # @param [Date] start_date
+  # @param [Date] end_date
+  # @return [Array<HeartRate>]
+  def heart_rate_time_series(start_date: Date.today, end_date: Date.today)
+    client.heart_rate_time_series(:start_date => start_date, :end_date => end_date)
+      .filter { |result| result["value"]["restingHeartRate"] }
+      .map { |result| HeartRate.from_result(result) }
+  end
+
+  # @param [Date] start_date
+  # @param [Date] end_date
+  # @return [Array<Steps>]
+  def steps_time_series(start_date: Date.today, end_date: Date.today)
+    client.activity_time_series("tracker/steps", :start_date => start_date, :end_date => end_date)
+      .filter { |result| result["value"] && result["value"] != "0" }
+      .map { |result| Steps.from_result(result) }
+  end
+
+private
+
+  attr_accessor :user_id
+
+  # @param [Hash] options
+  # @return [::FitbitAPI::Client]
+  def client(options = {})
+    conf = config(options)
+    @user_id = conf["user_id"]
+    @client ||= ::FitbitAPI::Client.new(conf)
+  end
+
+  # @param [Hash] options
+  #
+  # @option [String] user_id
+  # @option [String] access_token
+  # @option [String] refresh_token
+  # @option [Integer] expires_at
+  #
+  # @return [Hash]
+  def config(options = {})
+    options.merge(::Rails.application.config.fitbit).with_indifferent_access
+  end
+end
---
+++ web/lib/puller/oauth_client.rb
@@ -0,0 +1,21 @@
+class Puller::OauthClient
+  # Generates an authentication URL that grants an oauth code.
+  #
+  # @return [String]
+  def auth_url
+    raise NotImplementedError.new, "#auth_url is not implemented"
+  end
+
+  # Oauth code setter. Setting the code will trigger the Fitbit API client to
+  # load an oauth token which it will continue to use in subseqent API calls.
+  #
+  # @param [String] code
+  def code=(code)
+    raise NotImplementedError.new, "#code= is not implemented"
+  end
+
+  # @return [Hash]
+  def serialize_token
+    raise NotImplementedError.new, "#serialize_token is not implemented"
+  end
+end
