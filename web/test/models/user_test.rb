e7eae345d3f25210f17053c60c944023b6de9632
Marcos Minond <minond.marcos@gmail.com>
2020-02-24 00:56:20 -0700

    encryption and decryption tests

--- web/test/models/user_test.rb
+++ web/test/models/user_test.rb
@@ -5,4 +5,12 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(:email => "test1@test.com", :password => "xsj3k2lj4k3l2hio23321")
     assert user.save
   end
+
+  test "encryption and decryption" do
+    original = "testing123"
+    user = create(:user)
+    encrypted = user.encrypt_value(original)
+    decrypted = user.decrypt_value(encrypted)
+    assert_equal decrypted, original
+  end
 end
