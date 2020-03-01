a223637ce16d6e0b55da177e80d75ac4641067bf
Marcos Minond <minond.marcos@gmail.com>
2020-02-29 22:51:45 -0700

    logging from scheduler

--- web/config/initializers/scheduler.rb
+++ web/config/initializers/scheduler.rb
@@ -3,5 +3,6 @@ return if defined?(Rails::Console) || Rails.env.test? || ARGV.first != "jobs:wor
 puts "Initializing scheduler"
 
 Concurrent::TimerTask.new(:execution_interval => 2.hours) do
+  puts "scheduling ScheduleConnectionDataPullsJob"
   ScheduleConnectionDataPullsJob.perform_later
 end.execute
