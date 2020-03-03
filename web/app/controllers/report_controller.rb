5433cd0e1c8afd2ee48db8fd10e23497bda21a2c
Marcos Minond <minond.marcos@gmail.com>
2020-03-02 22:30:27 -0700

    report create

--- web/app/controllers/report_controller.rb
+++ web/app/controllers/report_controller.rb
@@ -35,6 +35,22 @@ class ReportController < ApplicationController
     locals :report => current_report
   end
 
+  # === URL
+  #   POST /report
+  #
+  # === Request fields
+  #   [String] report[label] - the report label
+  #
+  # === Sample request
+  #   /report
+  #
+  def create
+    report = create_report
+    ok = report.persisted?
+    notify(ok, :successful_extractor_create, :failure_in_extractor_create)
+    ok ? redirect_to(edit_report_path(report)) : locals(:new, :report => report)
+  end
+
 private
 
   param_reader :id
@@ -42,5 +58,18 @@ private
   # @return [Report]
   def current_report
     current_user.reports.find(id)
+  end
+
+  # @return [Report]
+  def create_report
+    extra = { :user => current_user }
+    attrs = permitted_report_params.to_hash.merge(extra)
+    Report.create(attrs)
+  end
+
+  # @return [ActionController::Parameters]
+  def permitted_report_params
+    params.require(:report)
+          .permit(:label)
   end
 end
