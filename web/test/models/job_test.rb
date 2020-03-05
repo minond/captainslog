ce629c884b62370403d8cb766a115c9434ce9eec
Marcos Minond <minond.marcos@gmail.com>
2020-03-04 21:47:27 -0700

    job schedule and arg encoding tests

--- web/test/models/job_test.rb
+++ web/test/models/job_test.rb
@@ -8,4 +8,31 @@ class JobTest < ActiveSupport::TestCase
                   :kind => :connection_data_pull_standard)
     assert job.save
   end
+
+  test "schedule! happy path" do
+    args = Job::ConnectionDataPullStandardArgs.new(:connection_id => 1)
+    assert Job.schedule!(user, :connection_data_pull_standard, args)
+  end
+
+  test "schedule! with invalid kind" do
+    args = Job::ConnectionDataPullStandardArgs.new(:connection_id => 1)
+    assert_raises(ArgumentError) { Job.schedule!(user, :connection_data_pull_standard_invalid, args) }
+  end
+
+  test "schedule! with invalid arguments" do
+    args = Job::ConnectionDataPullBackfillArgs.new(:connection_id => 1)
+    assert_raises(ArgumentError) { Job.schedule!(user, :connection_data_pull_standard, args) }
+  end
+
+  test "decoded_args" do
+    args = Job::ConnectionDataPullStandardArgs.new(:connection_id => 1)
+    job = Job.schedule!(user, :connection_data_pull_standard, args)
+    assert_equal job.decoded_args.connection_id, args.connection_id
+  end
+
+private
+
+  def user
+    @user ||= create(:user)
+  end
 end
