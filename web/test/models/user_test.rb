3ac4ce68b4fad4c99985cb845c50dabed3afe7d9
Marcos Minond <minond.marcos@gmail.com>
2020-03-04 20:40:21 -0700

    setting secret value for tests

--- web/test/models/user_test.rb
+++ web/test/models/user_test.rb
@@ -1,13 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  @original_secret_key_base = nil
+
+  setup do
+    @original_secret_key_base = Rails.application.credentials.secret_key_base
+    Rails.application.credentials.secret_key_base = "1" * 32
+  end
+
+  teardown do
+    Rails.application.credentials.secret_key_base = @original_secret_key_base
+  end
+
   test "save happy path" do
     user = User.new(:email => "test1@test.com", :password => "xsj3k2lj4k3l2hio23321")
     assert user.save
   end
 
   test "encryption and decryption" do
-    skip "Failing in CI, so skipping for now"
     original = "testing123"
     user = create(:user)
     encrypted = user.encrypt_value(original)
