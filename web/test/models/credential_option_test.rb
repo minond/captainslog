88bc182b07057f87202553ae443c22a46c687269
Marcos Minond <minond.marcos@gmail.com>
2020-03-04 21:21:27 -0700

    deleting unused comments

--- web/test/models/credential_option_test.rb
+++ web/test/models/credential_option_test.rb
@@ -24,43 +24,6 @@ class CredentialOptionTest < ActiveSupport::TestCase
     assert credential_option.decrypted_value == "value"
   end
 
-#   test "create_with_options creates an options for every key" do
-#     options = {
-#       :name1 => "Marcos1",
-#       :name2 => "Marcos2",
-#     }
-#
-#     assert_equal CredentialOption.count, 0
-#     Credential.create_with_options(user, connection, options)
-#     assert_equal CredentialOption.count, 2
-#   end
-#
-#   test "options returns all values" do
-#     options = {
-#       :name1 => "Marcos1",
-#       :name2 => "Marcos2",
-#     }
-#
-#     credential = Credential.create_with_options(user, connection, options)
-#     options = credential.options
-#     keys = options.keys
-#
-#     assert_equal %w[name1 name2].sort, keys.sort
-#   end
-#
-#   test "options returns decrypted values" do
-#     options = {
-#       :name1 => "Marcos1",
-#       :name2 => "Marcos2",
-#     }
-#
-#     credential = Credential.create_with_options(user, connection, options)
-#     options = credential.options
-#     values = options.values
-#
-#     assert_equal %w[Marcos1 Marcos2].sort, values.sort
-#   end
-
 private
 
   def user
