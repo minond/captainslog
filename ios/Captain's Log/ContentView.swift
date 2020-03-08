bbab155dd1d5d8374727cc3b1f09703fff46344c
Marcos Minond <minond.marcos@gmail.com>
2020-03-08 17:55:16 -0600

    ios native app wip

---
+++ ios/Captain's Log/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Captain's Log
+//
+//  Created by Marcos Minond on 3/7/20.
+//  Copyright © 2020 Marcos Minond. All rights reserved.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(
+  red: 239.0/255.0,
+  green: 243.0/255.0,
+  blue: 244.0/255.0, opacity: 1.0)
+
+struct ContentView: View {
+  @State private var email: String = ""
+  @State private var password: String = ""
+
+  var body: some View {
+    VStack {
+      Image("logo")
+        .padding(.top, 40)
+        .padding(.bottom, 60)
+      TextField("Email", text: $email)
+        .padding()
+        .background(lightGreyColor)
+        .cornerRadius(5.0)
+        .padding(.bottom, 20)
+      SecureField("Password", text: $password)
+        .padding()
+        .background(lightGreyColor)
+        .cornerRadius(5.0)
+        .padding(.bottom, 20)
+      Button(action: {print("Button tapped \(self.email) \(self.password)")}) {
+         Text("Continue")
+           .foregroundColor(.blue)
+           .padding()
+      }
+      Spacer()
+    }
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ ios/Captain's LogTests/Captain_s_LogTests.swift
@@ -0,0 +1,34 @@
+//
+//  Captain_s_LogTests.swift
+//  Captain's LogTests
+//
+//  Created by Marcos Minond on 3/7/20.
+//  Copyright © 2020 Marcos Minond. All rights reserved.
+//
+
+import XCTest
+@testable import Captain_s_Log
+
+class Captain_s_LogTests: XCTestCase {
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each
+    // test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of
+    // each test method in the class.
+  }
+
+  func testExample() {
+    // This is an example of a functional test case. Use XCTAssert and related
+    // functions to verify your tests produce the correct results.
+  }
+
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
+    }
+  }
+}
