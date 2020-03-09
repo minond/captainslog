a29bdae98f80a2051e88aab9d38f33abcbe699d8
Marcos Minond <minond.marcos@gmail.com>
2020-03-08 19:29:09 -0600

    minor style changes

--- ios/Captain's Log/SceneDelegate.swift
+++ ios/Captain's Log/SceneDelegate.swift
@@ -12,7 +12,11 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+      _ scene: UIScene,
+      willConnectTo session: UISceneSession,
+      options connectionOptions: UIScene.ConnectionOptions
+    ) {
       // Use this method to optionally configure and attach the UIWindow
       // `window` to the provided UIWindowScene `scene`. If using a storyboard,
       // the `window` property will automatically be initialized and attached
@@ -21,13 +25,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       // `application:configurationForConnectingSceneSession` instead).
 
       // Get the managed object context from the shared persistent container.
-      let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+      let context = (UIApplication.shared.delegate as! AppDelegate)
+        .persistentContainer.viewContext
 
       // Create the SwiftUI view and set the context as the value for the
       // managedObjectContext environment keyPath. Add
       // `@Environment(\.managedObjectContext)` in the views that will need the
       // context.
-      let contentView = ContentView().environment(\.managedObjectContext, context)
+      let contentView = ContentView()
+        .environment(\.managedObjectContext, context)
 
       // Use a UIHostingController as window root view controller.
       if let windowScene = scene as? UIWindowScene {
