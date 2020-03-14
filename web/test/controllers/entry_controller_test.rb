70648808e7fcefa511db18af01278a456bb2e9f0
Marcos Minond <minond.marcos@gmail.com>
2020-03-14 15:38:31 -0600

    testing entry controller

--- web/test/controllers/entry_controller_test.rb
+++ web/test/controllers/entry_controller_test.rb
@@ -3,7 +3,27 @@ require "test_helper"
 class EntryControllerTest < ActionDispatch::IntegrationTest
   setup { sign_in user }
 
-  test "it can destroy an entry owned" do
+  test "it can show an entry" do
+    entry = create(:entry, :user => user)
+    get "/book/#{entry.book.slug}/entry/#{entry.id}"
+    assert_response :success
+  end
+
+  test "is can create an entry and schedules processing" do
+    assert_enqueued_jobs 1 do
+      post "/book/#{create(:book, :user => user).slug}/entry", :params => { :text => "hi" }
+    end
+  end
+
+  test "it can update entries and scheduled reprocessins" do
+    assert_enqueued_jobs 2 do
+      entry = create(:entry, :user => user, :original_text => "original")
+      patch "/book/#{entry.book.slug}/entry/#{entry.id}", :params => { :original_text => "update" }
+      entry.reload
+    end
+  end
+
+  test "it can destroy an entry" do
     entry = create(:entry, :user => user)
     delete "/book/#{entry.book.slug}/entry/#{entry.id}"
     assert_response :redirect
