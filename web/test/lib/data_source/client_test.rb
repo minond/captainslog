4922da255c524474af9c3ba0f3a835cff1328b5f
Marcos Minond <minond.marcos@gmail.com>
2020-03-14 17:38:08 -0600

    data source client tests

---
+++ web/test/lib/data_source/client_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class DataSourceClientTest < ActiveSupport::TestCase
+  test "state encoding/decoding" do
+    user = create(:user)
+    book = create(:book, :user => user)
+    connection = create(:connection, :user => user, :book => book)
+    encoded = DataSource::Client.encode_state(connection)
+    connection_id, _rest = DataSource::Client.decode_state(encoded)
+    assert connection.id
+    assert_equal connection.id, connection_id
+  end
+
+  test "needs base_auth_url override" do
+    assert_raises(NotImplementedError) { DataSource::Client.new.base_auth_url }
+  end
+
+  test "needs credential_options override" do
+    assert_raises(NotImplementedError) { DataSource::Client.new.credential_options }
+  end
+
+  test "needs data_pull override" do
+    assert_raises(NotImplementedError) { DataSource::Client.new.send(:data_pull, {}) }
+  end
+end
