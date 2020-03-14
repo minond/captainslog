0ff7fb86a798f44586a8b14863c83ef2d0e101e5
Marcos Minond <minond.marcos@gmail.com>
2020-03-14 17:27:14 -0600

    fitbit results

--- web/test/lib/data_source/fitbit_test.rb
+++ web/test/lib/data_source/fitbit_test.rb
@@ -12,7 +12,87 @@ class DataSourceFibitTest < ActiveSupport::TestCase
     client.data_pull_backfill
   end
 
+  test "parse valid heart rate results" do
+    heart_rate_results = [
+      heart_rate_result(2.days.ago, 80),
+      heart_rate_result(3.days.ago, 81),
+      heart_rate_result(4.days.ago, 82),
+    ]
+    results = { :heart_rate_results => heart_rate_results }
+    entries = client(:results => results).send(:data_pull)
+    assert_equal 3, entries.size
+  end
+
+  test "ignore invalid heart rate results" do
+    heart_rate_results = [{}, heart_rate_result(3.days.ago, 81), {}]
+    results = { :heart_rate_results => heart_rate_results }
+    entries = client(:results => results).send(:data_pull)
+    assert_equal 1, entries.size
+  end
+
+  test "parse valid step results" do
+    steps_results = [
+      steps_result(2.days.ago, 1234),
+      steps_result(3.days.ago, 1233),
+      steps_result(4.days.ago, 1232),
+    ]
+    results = { :activity_results => steps_results }
+    entries = client(:results => results).send(:data_pull)
+    assert_equal 3, entries.size
+  end
+
+  test "ignore invalid step results" do
+    steps_results = [{}, steps_result(3.days.ago, 4321), {}]
+    results = { :activity_results => steps_results }
+    entries = client(:results => results).send(:data_pull)
+    assert_equal 1, entries.size
+  end
+
+  test "parse valid weight results" do
+    weight_results = [
+      weight_result(2.days.ago, 151),
+      weight_result(3.days.ago, 152),
+      weight_result(4.days.ago, 153),
+    ]
+    results = { :weight_results => weight_results }
+    entries = client(:results => results).send(:data_pull)
+    assert_equal 3, entries.size
+  end
+
+  test "ignore invalid weight results" do
+    weight_results = [{}, weight_result(3.days.ago, 152), {}]
+    results = { :weight_results => weight_results }
+    entries = client(:results => results).send(:data_pull)
+    assert_equal 1, entries.size
+  end
+
 private
+
+  def heart_rate_result(date, value)
+    {
+      "dateTime" => date.to_s,
+      "value" => {
+        "restingHeartRate" => value,
+      }
+    }
+  end
+
+  def steps_result(date, value)
+    {
+      "dateTime" => date.to_s,
+      "value" => value,
+    }
+  end
+
+  def weight_result(date, weight = nil, bmi = nil)
+    {
+      "logId" => SecureRandom.uuid,
+      "date" => date.to_date.to_s,
+      "time" => date.to_time.to_s.split.second,
+      "weight" => weight,
+      "bmi" => bmi,
+    }
+  end
 
   def client(options: {}, results: {})
     @client ||=
