b5e0664bde1574f9fefab1bd77bcd3b9f6849205
Marcos Minond <minond.marcos@gmail.com>
2020-03-21 16:06:37 -0600

    moving tracer builder into lib

--- web/config/initializers/opentracing.rb
+++ web/config/initializers/opentracing.rb
@@ -7,23 +7,4 @@ Rails.application.config.jaeger = conf.with_indifferent_access
 
 return if Rails.env.test?
 
-service_name = ARGV.first == "jobs:work" ? "worker" : "web"
-
-OpenTracing.global_tracer = Jaeger::Client.build(
-  :host => Rails.application.config.jaeger[:host],
-  :port => Rails.application.config.jaeger[:port],
-  :service_name => service_name,
-  :sampler => Jaeger::Samplers::Const.new(true),
-  :reporter => Jaeger::Client::Reporters::RemoteReporter.new(
-    :flush_interval => Rails.application.config.jaeger[:flush_interval],
-    :sender => Jaeger::UdpSender.new(
-      :host => Rails.application.config.jaeger[:host],
-      :port => Rails.application.config.jaeger[:port],
-      :max_packet_size => Rails.application.config.jaeger[:max_packet_size],
-      :logger => Logger.new(STDOUT),
-      :encoder => Jaeger::Encoders::ThriftEncoder.new(
-        :service_name => service_name
-      )
-    )
-  )
-)
+OpenTracing.global_tracer = Tracing.build_tracer
