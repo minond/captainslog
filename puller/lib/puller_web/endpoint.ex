99c9e518bfa5d35f4994885acd2f4a3caa04ce00
Marcos Minond <minond.marcos@gmail.com>
2020-04-18 17:05:23 -0600

    user model and authentication

--- puller/lib/puller_web/endpoint.ex
+++ puller/lib/puller_web/endpoint.ex
@@ -43,5 +43,6 @@ defmodule PullerWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :puller
   plug PullerWeb.Router
 end
--- puller/lib/puller_web/router.ex
+++ puller/lib/puller_web/router.ex
@@ -1,5 +1,6 @@
 defmodule PullerWeb.Router do
   use PullerWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -9,18 +10,24 @@ defmodule PullerWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :authenticated do
+    plug Pow.Plug.RequireAuthenticated,
+      error_handler: Pow.Phoenix.PlugErrorHandler
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", PullerWeb do
+  scope "/" do
     pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", PullerWeb do
+    pipe_through [:browser, :authenticated]
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", PullerWeb do
-  #   pipe_through :api
-  # end
 end
