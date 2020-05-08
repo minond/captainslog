2ed72e6c6b50087ed8c64e4fed52f2fe8385fdc0
Marcos Minond <minond.marcos@gmail.com>
2020-05-07 18:19:10 -0600

    singular form routes for user resource

--- puller/config/routes.rb
+++ puller/config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   root "home#home"
 
-  devise_for :users, :only => [:sessions]
+  devise_for :user, :only => [:sessions]
 
-  # cancel_user_registration GET    /users/cancel(.:format)   devise/registrations#cancel
-  #    new_user_registration GET    /users/sign_up(.:format)  devise/registrations#new
-  #   edit_user_registration GET    /users/edit(.:format)     devise/registrations#edit
-  #        user_registration PATCH  /users(.:format)          devise/registrations#update
-  #                          PUT    /users(.:format)          devise/registrations#update
-  #                          DELETE /users(.:format)          devise/registrations#destroy
-  #                          POST   /users(.:format)          devise/registrations#create
+  # cancel_user_registration GET    /user/cancel(.:format)   devise/registrations#cancel
+  #    new_user_registration GET    /user/sign_up(.:format)  devise/registrations#new
+  #   edit_user_registration GET    /user/edit(.:format)     devise/registrations#edit
+  #        user_registration PATCH  /user(.:format)          devise/registrations#update
+  #                          PUT    /user(.:format)          devise/registrations#update
+  #                          DELETE /user(.:format)          devise/registrations#destroy
+  #                          POST   /user(.:format)          devise/registrations#create
   as :user do
-    get "users/sign_up" => "devise/registrations#new", :as => :new_user_registration
-    post "users" => "devise/registrations#create", :as => :user_registration
+    get "user/sign_up" => "devise/registrations#new", :as => :new_user_registration
+    post "user" => "devise/registrations#create", :as => :user_registration
   end
 
   authenticated :user do
