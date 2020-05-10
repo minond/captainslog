6806bc8bf0462794fbe6af7812d6a5bd5a49fc7a
Marcos Minond <minond.marcos@gmail.com>
2020-05-10 17:38:51 -0600

    user encryption helpers

--- puller/app/models/user.rb
+++ puller/app/models/user.rb
@@ -14,6 +14,18 @@ class User < ApplicationRecord
     "https://www.gravatar.com/avatar/#{email_hash}?d=blank"
   end
 
+  # @param [String] value
+  # @return [String]
+  def encrypt_value(value)
+    encryptor.encrypt_and_sign(value)
+  end
+
+  # @param [String] value
+  # @return [String]
+  def decrypt_value(value)
+    encryptor.decrypt_and_verify(value)
+  end
+
 private
 
   def constructor
@@ -23,5 +35,18 @@ private
   # @return [String]
   def email_hash
     Digest::MD5.hexdigest(email.downcase)
+  end
+
+  # @return [String]
+  def key
+    len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.credentials.secret_key_base
+    generator = ActiveSupport::KeyGenerator.new(secret)
+    generator.generate_key(salt, len)
+  end
+
+  # @return [ActiveSupport::MessageEncryptor]
+  def encryptor
+    ActiveSupport::MessageEncryptor.new(key)
   end
 end
