78e5feee7a361bce730d13a02d9918b74a6bfde3
Marcos Minond <minond.marcos@gmail.com>
2020-05-10 17:38:16 -0600

    credential and credential option migration

--- puller/db/schema.rb
+++ puller/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_000359) do
+ActiveRecord::Schema.define(version: 2020_05_10_233343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,24 @@ ActiveRecord::Schema.define(version: 2020_05_08_000359) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_connections_on_user_id"
+  end
+
+  create_table "credential_options", force: :cascade do |t|
+    t.bigint "credential_id", null: false
+    t.string "label"
+    t.text "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["credential_id"], name: "index_credential_options_on_credential_id"
+  end
+
+  create_table "credentials", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "connection_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["connection_id"], name: "index_credentials_on_connection_id"
+    t.index ["user_id"], name: "index_credentials_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +52,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_000359) do
   end
 
   add_foreign_key "connections", "users"
+  add_foreign_key "credential_options", "credentials"
+  add_foreign_key "credentials", "connections"
+  add_foreign_key "credentials", "users"
 end
