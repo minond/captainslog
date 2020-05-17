3ab767e13d7ddf997d392998f5175c77671d9f1d
Marcos Minond <minond.marcos@gmail.com>
2020-05-16 22:47:39 -0600

    smaller broadcast payloads

--- puller/app/channels/job_channel.rb
+++ puller/app/channels/job_channel.rb
@@ -1,11 +1,11 @@
 class JobChannel < ApplicationCable::Channel
   def subscribed
     stream_from "jobs_for_user_#{current_user.id}", :coder => ActiveSupport::JSON do |payload|
-      job = Job.new(payload["job"])
-      connection = Connection.new(payload["connection"])
+      job = Job.find(payload["job_id"])
+      connection = Connection.find(payload["connection_id"])
 
-      transmit :job => payload["job"],
-               :connection => payload["connection"],
+      transmit :job => { :id => job.id },
+               :connection => { :id => connection.id },
                :job_row_html => render("job/row", :job => job),
                :connection_row_html => render("connection/row", :connection => connection)
     end
--- puller/app/models/job.rb
+++ puller/app/models/job.rb
@@ -31,7 +31,7 @@ private
   end
 
   def broadcast_record
-    ActionCable.server.broadcast("jobs_for_user_#{user.id}", :job => self,
-                                                             :connection => connection)
+    ActionCable.server.broadcast("jobs_for_user_#{user.id}", :job_id => id,
+                                                             :connection_id => connection.id)
   end
 end
