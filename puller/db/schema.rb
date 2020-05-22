c0bbdbaa16e67d5188898b9cd46c25803ee978d4
Marcos Minond <minond.marcos@gmail.com>
2020-05-22 13:31:41 -0600

    user.remember_created_at

--- puller/db/schema.rb
+++ puller/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_054359) do
+ActiveRecord::Schema.define(version: 2020_05_22_193053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_05_16_054359) do
     t.text "salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
