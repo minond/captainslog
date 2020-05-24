e65db1f4c010c7ba8785f6aa0b71bca83efbce91
Marcos Minond <minond.marcos@gmail.com>
2020-05-23 21:31:21 -0600

    definiting available output destinations for cl client

--- puller/app/services/source/captainslog.rb
+++ puller/app/services/source/captainslog.rb
@@ -20,4 +20,48 @@ class Source::Captainslog < Source::Client
   def credential_options
     { :token => token }
   end
+
+  # @return [Array<Source::Client::Output::Destination>]
+  def available_output_destinations
+    books.map do |book|
+      Output::Destination.new(:id => book["id"], :label => book["name"])
+    end
+  end
+
+private
+
+  # @return [Hash]
+  def books
+    make(request(:get, "/api/v1/books"))
+  end
+
+  # @param [Net::HTTPRequest] req
+  # @return [Hash]
+  def make(req)
+    res = client.request(req)
+    JSON.parse(res.body)
+  end
+
+  # @param [Symbol] method
+  # @param [String] path
+  # @return [Net::HTTPRequest]
+  def request(method, path)
+    req = case method
+          when :get
+            Net::HTTP::Get.new(path)
+          end
+
+    req["Authorization"] = token
+    req
+  end
+
+  # @param [Hash] options
+  # @return [Net::HTTP]
+  def client(options = {})
+    @client ||= begin
+                  conf = config(options)
+                  uri = URI.parse(conf[:application_uri])
+                  Net::HTTP.new(uri.host, uri.port)
+                end
+  end
 end
