514c4192a65f3d122bdab131c5e7dc36907034f5
Marcos Minond <minond.marcos@gmail.com>
2020-05-24 19:16:28 -0600

    homepage locals

--- puller/app/controllers/home_controller.rb
+++ puller/app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def home
     if current_user
-      locals :user => current_user
+      locals :connections => current_user.connections.order("service"),
+             :jobs => current_user.jobs.order("created_at desc").first(100)
     else
       redirect_to :new_user_session
     end
