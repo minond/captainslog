64abaa86f23145d81571a0263dbd6c63b9599041
Marcos Minond <minond.marcos@gmail.com>
2020-05-25 17:30:28 -0600

    job view components

---
+++ puller/app/components/job_row_component.rb
@@ -0,0 +1,28 @@
+class JobRowComponent < Component
+  props :job => Job
+
+  # rubocop:disable Metrics/AbcSize
+  def render
+    <<-HTML
+      <tr data-job-id="#{job.id}">
+        <td class="nowrap pv3 ph3 bb b--black-10 tc">#{job_status}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{job.presenter.kind}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{job.message}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{job.presenter.run_time}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{job.created_at}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{job.started_at}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{job.stopped_at}</td>
+        <td class="nowrap pv3 pr3 bb b--black-10">#{details_link}</td>
+      </tr>
+    HTML
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def job_status
+    JobStatusComponent.render(:id => job.id, :status => job.status)
+  end
+
+  def details_link
+    link_to t(:view_details), job_path(job), :class => "link blue"
+  end
+end
