2613d2d5325ec8c337063afe975fc845323f0a27
Marcos Minond <minond.marcos@gmail.com>
2020-05-25 22:00:12 -0600

    job view components

---
+++ puller/app/components/job_logs_component.rb
@@ -0,0 +1,13 @@
+class JobLogsComponent < Component
+  props :job => Job
+
+  def render
+    <<-HTML
+      <pre class="bg-near-white pa3 mt4 pre job-logs" data-job-details-logs>#{logs_string}</pre>
+    HTML
+  end
+
+  def logs_string
+    job.logs || t(:no_logs)
+  end
+end
---
+++ puller/app/components/job_show_component.rb
@@ -0,0 +1,22 @@
+class JobShowComponent < Component
+  props :job => Job
+
+  def render
+    ContentComponent.render { content + javascript }
+  end
+
+  def content
+    <<-HTML
+      <div data-job-details="#{job.id}">
+        #{JobDetailsComponent.render(:job => job)}
+        #{JobLogsComponent.render(:job => job)}
+      </div>
+    HTML
+  end
+
+  def javascript
+    <<-HTML
+      <script src="/assets/job.js"></script>
+    HTML
+  end
+end
