97490765577e575646eb456d193cebd19ce7c079
Marcos Minond <minond.marcos@gmail.com>
2020-05-25 22:08:26 -0600

    clearning up job details component

--- puller/app/components/job_details_component.rb
+++ puller/app/components/job_details_component.rb
@@ -1,40 +1,36 @@
 class JobDetailsComponent < Component
   props :job => Job
 
+  # rubocop:disable Metrics/AbcSize
   def render
+    table [
+      row(:status, job.presenter.status),
+      row(:kind, job.presenter.kind),
+      row(:message, job.message),
+      row(:run_time, job.presenter.run_time),
+      row(:created_at, job.created_at),
+      row(:started_at, job.started_at),
+      row(:stopped_at, job.stopped_at),
+    ]
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def table(rows)
     <<-HTML
       <table class="f6 w-50-l">
         <tbody class="lh-copy">
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:status)}</td>
-            <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-status>#{job.presenter.status}</td>
-          </tr>
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:kind)}</td>
-            <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-kind>#{job.presenter.kind}</td>
-          </tr>
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:message)}</td>
-            <td class="pv2 ph3 bb b--black-10 mw7" data-job-details-message>#{job.message}</td>
-          </tr>
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:run_time)}</td>
-            <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-run_time>#{job.presenter.run_time}</td>
-          </tr>
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:created_at)}</td>
-            <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-created_at>#{job.created_at}</td>
-          </tr>
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:started_at)}</td>
-            <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-started_at>#{job.started_at}</td>
-          </tr>
-          <tr>
-            <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(:stopped_at)}</td>
-            <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-stopped_at>#{job.stopped_at}</td>
-          </tr>
+          #{rows.join}
         </tbody>
       </table>
+    HTML
+  end
+
+  def row(field, value)
+    <<-HTML
+      <tr>
+        <td class="pv2 ph3 bb b--black-10 nowrap w2 br b tr">#{t(field)}</td>
+        <td class="pv2 ph3 bb b--black-10 nowrap" data-job-details-#{field}>#{value}</td>
+      </tr>
     HTML
   end
 end
