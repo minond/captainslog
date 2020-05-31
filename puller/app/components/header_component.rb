1758aba93a212ab2b65c21bcd63fcc9d160049a7
Marcos Minond <minond.marcos@gmail.com>
2020-05-30 21:38:01 -0600

    header component takes args now, also adding new strings for vertex views

--- puller/app/components/header_component.rb
+++ puller/app/components/header_component.rb
@@ -1,10 +1,11 @@
 class HeaderComponent < Component
   props :key => Symbol
+        :args => Object
 
   def render
     <<-HTML
       <div class="f3 baskerville mb3">
-        #{t(:settings)}
+        #{t(key, **(args || {}))}
       </div>
     HTML
   end
