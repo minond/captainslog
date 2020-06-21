dd87fce8dfa050dd55119bdda4f5230c83ac1e8c
Marcos Minond <minond.marcos@gmail.com>
2020-06-21 14:26:04 -0600

    selected source methods in source clients

--- puller/app/connections/service/client/source.rb
+++ puller/app/connections/service/client/source.rb
@@ -22,6 +22,14 @@ module Service::Client::Source
       @available_source_resources
     end
 
+    # @param [Array<Symbol>] resources
+    # @return [Array<Service::Resource>]
+    def selected_source_resources(selected_resources)
+      available_source_resources.select do |ty|
+        selected_resources.include?(ty.id)
+      end
+    end
+
     # @param [Range<ActiveSupport::Duration>] range
     def backfill_range(range)
       @backfill_range = range
@@ -35,6 +43,14 @@ module Service::Client::Source
     # @return [Date]
     def backfill_range_end_date
       @backfill_range.last.from_now.to_date
+    end
+
+    # @return [Hash]
+    def backfill_range_dates
+      {
+        :start_date => backfill_range_start_date,
+        :end_date => backfill_range_end_date,
+      }
     end
 
     # @param [Range<ActiveSupport::Duration>] range
@@ -51,26 +67,39 @@ module Service::Client::Source
     def standard_range_end_date
       @standard_range.last.from_now.to_date
     end
-  end
 
-  # @yieldparam [Service::Record]
-  def pull(**args, &block)
-    self.class.available_source_resources.each do |ty|
-      send("pull_#{ty.id}", args, &block)
+    # @return [Hash]
+    def standard_range_dates
+      {
+        :start_date => standard_range_start_date,
+        :end_date => standard_range_end_date,
+      }
     end
   end
 
+  # @param [Hash] options
+  # @option [Date] start_date
+  # @option [Date] end_date
+  # @option [Array<Symbol>] resources
   # @yieldparam [Service::Record]
-  def pull_backfill(&block)
-    pull(:start_date => self.class.backfill_range_start_date,
-         :end_date => self.class.backfill_range_end_date,
-         &block)
+  def pull(**options, &block)
+    resources = options.delete(:resources) || {}
+    self.class.selected_source_resources(resources).each do |ty|
+      send("pull_#{ty.id}", options, &block)
+    end
   end
 
+  # @param [Hash] options
+  # @option [Array<Symbol>] resources
   # @yieldparam [Service::Record]
-  def pull_standard(&block)
-    pull(:start_date => self.class.standard_range_start_date,
-         :end_date => self.class.standard_range_end_date,
-         &block)
+  def pull_backfill(**options, &block)
+    pull(options.merge(self.class.backfill_range_dates), &block)
+  end
+
+  # @param [Hash] options
+  # @option [Array<Symbol>] resources
+  # @yieldparam [Service::Record]
+  def pull_standard(**options, &block)
+    pull(options.merge(self.class.standard_range_dates), &block)
   end
 end
