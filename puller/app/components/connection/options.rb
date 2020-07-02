266b825addfb84e2edb977dd8e8ea7bc72472754
Marcos Minond <minond.marcos@gmail.com>
2020-07-02 00:15:05 -0600

    propagating view context

--- puller/app/components/connection/options.rb
+++ puller/app/components/connection/options.rb
@@ -8,7 +8,7 @@ class Connection::Options < ViewComponent
         <a class="link black pointer fl mw6 pa2 border-box" href="/connection/initiate/#{service}">
           <input class="service-radio dn" type="radio" name="connection[service]" value="#{service}" />
           <div class="service-box pa4 ba b--black-10 tc">
-            <img src="#{"/assets/#{service}-logo.#{ext}"}" class="h3" />
+            <img src="#{asset_path("#{service}-logo.#{ext}")}" class="h3" />
             <p class="lh-copy">#{t(:"#{service}_connection_description")}</p>
           </div>
         </a>
@@ -26,7 +26,9 @@ class Connection::Options < ViewComponent
 
   def options
     [:captainslog, :fitbit, %i[lastfm svg]].map do |(service, ext)|
-      Option.render(:service => service, :ext => ext || :png)
+      Option.render(:view_context => view_context,
+                    :service => service,
+                    :ext => ext || :png)
     end
   end
 end
